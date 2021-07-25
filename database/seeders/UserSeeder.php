cbd2efe82489f2d40a38f7f046290da120c2b7da
genstail24 <genstail24@gmail.com>
2021-07-26 02:17:11 +0700

    adding is_admin column to user table

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,14 +20,16 @@ class UserSeeder extends Seeder
                 'email' => 'user@gmail.com',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10)
+                'remember_token' => Str::random(10),
+                'is_admin' => false
             ],
             [
                 'name' => 'admin',
                 'email' => 'admin@gmail.com',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'remember_token' => Str::random(10)
+                'remember_token' => Str::random(10),
+                'is_admin' => true
             ]
         ];
         
